34963111964c11ee1b00a8856a3146ac3f36a83d
Nayan Ghosh <138111956+nayanghosh-2002@users.noreply.github.com>
2025-07-11 04:49:10 +0530

    commit

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -9,6 +9,7 @@ const io = new Server(server, {
     cors:{
         origin: [
             "http://localhost:3000",
+            "https://chatify-peach.vercel.app",
             "https://chatify-frontend-r27k.onrender.com" 
         ],
         credentials: true,
