bee5607f980ef5e418d3f07b5867dbafb6d0b5f7
Nayan Ghosh <138111956+nayanghosh-2002@users.noreply.github.com>
2025-07-11 05:15:52 +0530

    commit

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,9 +8,9 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors:{
         origin: [
-            "http://localhost:3000",
-            "https://chatify-peach.vercel.app",
-            "https://chatify-frontend-r27k.onrender.com" 
+            
+            "https://chatify-peach.vercel.app"
+            
         ],
         credentials: true,
         methods:['GET', 'POST'],
