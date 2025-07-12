520b1dc9bdfdc12c7bbd47336c5cce6f8ef8eb9e
Nayan Ghosh <138111956+nayanghosh-2002@users.noreply.github.com>
2025-07-13 00:46:08 +0530

    commit

--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { getMessage, sendMessage } from "../controllers/messageController.js";
-import isAuthenticated from "../middleware/isAuthenticated.js";
+import verifyToken from "../middleware/verifyToken.js"; // <-- New import
 
 const router = express.Router();
 
-router.route("/send/:id").post(isAuthenticated,sendMessage);
-router.route("/:id").get(isAuthenticated, getMessage);
+router.route("/send/:id").post(verifyToken,sendMessage); // <-- New usage
+router.route("/:id").get(verifyToken, getMessage); // <-- New usage
 
 export default router;
