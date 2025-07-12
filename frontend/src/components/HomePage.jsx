b03918d430a65e4688353542523dcb104f90e9c6
Nayan Ghosh <138111956+nayanghosh-2002@users.noreply.github.com>
2025-07-13 02:21:44 +0530

    commit

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -13,11 +13,11 @@ const HomePage = () => {
     }
   }, [authUser, navigate]);
   return (
-    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+    <div className='flex flex-col sm:flex-row sm:h-[450px] md:h-[550px] w-full md:w-[80%] lg:w-[70%] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
       <Sidebar />
       <MessageContainer />
     </div>
   )
 }
 
-export default HomePage
+export default HomePage;
